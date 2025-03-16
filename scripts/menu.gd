d849e70c678af6af464c6938be46c2d7a8c2afb6
Shelseymp <shelseymp@gmail.com>
2025-03-15 21:16:55 -0700

    .

--- scripts/menu.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
-
-
-func _on_load_pressed() -> void:
-	pass # Replace with function body.
