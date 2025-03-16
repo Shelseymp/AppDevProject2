ede096a9019006dcd6d2ecdfaa078035245f49fb
Shelseymp <shelseymp@gmail.com>
2025-03-16 15:35:16 -0700

    code for the states agro and wander

--- scripts/vilgax_battle.gd
+++ scripts/vilgax_battle.gd
@@ -6,3 +6,12 @@ extends CharacterBody2D
 @onready var player_track_raycast = $PlayerTrackerPivot/Player_Track_Raycast as RayCast2D
 
 @onready var player_tracker_pivot = $PlayerTrackerPivot as Node2D
+
+@export var wander_speed : float = 40
+@export var chase_speed : float = 80.0
+
+var current_speed : float = 0.0
+var player_found : bool = false
+
+func _ready() -> void:
+	pass
