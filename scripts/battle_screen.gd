3926ca3feea50265def7f9a88f021f9f468d8da1
Shelseymp <shelseymp@gmail.com>
2025-03-18 08:14:11 -0700

    updating text

--- scripts/battle_screen.gd
+++ scripts/battle_screen.gd
@@ -1,14 +1,22 @@
 extends Node2D
 
 @onready var playerpop = $PlayerDied
+@onready var battle_text: Label = $BattleText
 
 @onready var enemypop = $EnemyDied
 
 func _ready() -> void:
+	BattleStart()
 	Global.playerAlive = true
 	Global.Defeated1 = false
 	enemypop.hide()
 	playerpop.hide()
+	
+func BattleStart():
+	await get_tree().create_timer(0.5).timeout
+	battle_text.show()
+	await get_tree().create_timer(2.0).timeout
+	battle_text.hide()
 	
 func _physics_process(delta):
 	WhoDied()
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
+@onready var score_label: Label = $ScoreLabel
+
+var theScore = Global.Score
+
 func _physics_process(delta):
 	print(Global.Score)
+	the_score()
+	
+func the_score():
+	score_label.text = "Your score is : " + str(theScore) + " !"
+	
