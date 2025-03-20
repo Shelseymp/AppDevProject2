5694bc45c1c677700b4a5201b6cea45a28d53bef
vfigueroa721 <vfigueroa7@csub.edu>
2025-03-20 13:44:46 -0700

    add the scores to a file
    save scores to file

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -1,5 +1,21 @@
 extends Node
 
+var player_score = 0
+
+func save_score():
+	var file = FileAccess.open("user://score_data.json", FileAccess.WRITE)
+	var data = {"score": player_score}
+	file.store_string(JSON.stringify(data))
+	file.close()
+
+func load_score():
+	if FileAccess.file_exists("user://score_data.json"):
+		var file = FileAccess.open("user://score_data.json", FileAccess.READ)
+		var data = JSON.parse_string(file.get_as_text())
+		if data and "score" in data:
+			player_score = data["score"]
+		file.close()
+
 var playerBody : CharacterBody2D
 var playerWeaponsEquip:bool
 
